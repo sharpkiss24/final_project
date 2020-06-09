ab6393743f1bd3a055e53b933baba8a127dcf22a
cherry <41586167+sharpkiss24@users.noreply.github.com>
2020-06-09 19:52:20 -0400

    my comment

--- Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/MainActivity.java
+++ Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//hi this is cherry
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
