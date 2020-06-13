2136798340ff4e223b38b44a89df1634146cc145
Cyd <cydhaynes0@gmail.com>
2020-06-13 12:25:58 -0400

    New Layout Files for App

---
+++ Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/BasketballActivity.java
@@ -0,0 +1,14 @@
+package com.example.pro_athletestats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasketballActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basketball);
+    }
+}
--- Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/MainActivity.java
+++ Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.pro_athletestats;
-//Hellooooo Partner!!!!
 
-//Woody from Toy Storyyy!!!!
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-//hi this is cherry
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openBasketballActivity(View view) {
+        Intent intentToOpenArtistActivity = new Intent(this, BasketballActivity.class);
+        startActivity(intentToOpenArtistActivity);
+    }
+
+    public void openBaseballActivity(View view) {
+        Intent intentToOpenAlbumActivity = new Intent(this, BaseballActivity.class);
+        startActivity(intentToOpenAlbumActivity);
+    }
+
+    public void openFootballActivity(View view) {
+        Intent intentToOpenAlbumActivity = new Intent(this, FootballActivity.class);
+        startActivity(intentToOpenAlbumActivity);
+    }
+
+    public void openSoccerActivity(View view) {
+        Intent intentToOpenAlbumActivity = new Intent(this, SoccerActivity.class);
+        startActivity(intentToOpenAlbumActivity);
+    }
+
 }
