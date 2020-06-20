042d068796ac18517e0dbde9ed47b174d381d806
Cyd <cydhaynes0@gmail.com>
2020-06-20 12:35:40 -0400

    Tried adding API

--- Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/MainActivity.java
+++ Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.pro_athletestats;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,4 +57,31 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intentToOpenAlbumActivity);
     }
 
+/*    final TextView textView = (TextView) findViewById(R.id.text);
+// ...
+
+    // Instantiate the RequestQueue.
+    RequestQueue queue = Volley.newRequestQueue(this);
+    String url ="http://www.google.com";
+
+    // Request a string response from the provided URL.
+    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    // Display the first 500 characters of the response string.
+                    textView.setText("Response is: "+ response.substring(0,500));
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            textView.setText("That didn't work!");
+        }
+    });
+
+// Add the request to the RequestQueue.
+  //  queue.add(stringRequest);
+*/
 }
+
+
