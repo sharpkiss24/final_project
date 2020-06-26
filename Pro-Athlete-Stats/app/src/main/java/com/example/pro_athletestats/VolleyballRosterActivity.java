beedcdf1f7e939cf57966f07841ee3a75c418de5
Cyd <cydhaynes0@gmail.com>
2020-06-26 03:08:02 -0400

    Updated XML, Java and XML files for all sports are complete

---
+++ Pro-Athlete-Stats/app/src/main/java/com/example/pro_athletestats/VolleyballRosterActivity.java
@@ -0,0 +1,14 @@
+package com.example.pro_athletestats;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VolleyballRosterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volleyball_roster);
+    }
+}
